1a1d2d0fc1e1df96b3be0d4fc928910ada7096d2
IOgea <Irogea@gmail.com>
2020-10-14 15:10:13 -0500

    partially completed favExercises

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,8 +1,14 @@
+
 var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+ for (i = 0; i<= exercises.length-1;i++){
+   selExercises.addItem(exercises[i])
+   }
+for (i = 0;i<=coreExercises.length-1;i++){
+  selExercises.addItem(coreExercises[i])
+  }
 }
 
 selExercises.onfocusout=function(){
@@ -10,3 +16,7 @@ selExercises.onfocusout=function(){
 }
 
 
+
+btnSubmit.onclick=function(){
+  
+}
